3e0084ad78c7d46059e1041ff95ba53689e7c5ef
ixboy <ismaelixboy@gmail.com>
2022-06-16 00:41:10 +0300

    remove second  solutions

--- solutions_query.sql
+++ solutions_query.sql
@@ -1,21 +1,8 @@
 -- First Query
-SELECT
-  COUNT(*)
-FROM
-  visits
-where
-  animal_id = 4;
+SELECT COUNT(*) FROM visits where animal_id = 4;
 
 -- Solution for first query
 CREATE INDEX animals_visits_id ON visits(animal_id ASC);
 
 --second query
-SELECT
-  *
-FROM
-  visits
-where
-  vet_id = 2;
 
---Solution for first query
-CREATE INDEX vet_id_asc ON visits(vet_id ASC);
