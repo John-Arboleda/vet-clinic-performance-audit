d1aa0954dd4ba97dcfcd64c6428a536bc8dad1cb
ixboy <ismaelixboy@gmail.com>
2022-06-16 00:43:36 +0300

    add optimized solutions for last query

--- solutions_query.sql
+++ solutions_query.sql
@@ -1,8 +1,21 @@
 -- First Query
-SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT
+  COUNT(*)
+FROM
+  visits
+where
+  animal_id = 4;
 
 -- Solution for first query
 CREATE INDEX animals_visits_id ON visits(animal_id ASC);
 
---second query
+--last query
+SELECT
+  *
+FROM
+  owners
+where
+  email = 'owner_18327@mail.com';
 
+-- to optimize the time execution and performance for the last query we used the following query
+CREATE INDEX email_asc ON owners(email ASC);
